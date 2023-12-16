580141dabecde1a8d8d29e02fa32a60d38cf9486
Luna2503 <lchkty@gmail.com>
2023-12-16 13:26:14 +0530

    Netflix project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Row from './Components/Row';
+import Banner from './Components/Banner';
+import Navigationbar from './Components/Navigationbar';
+import { requests } from './request';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigationbar/>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title='Netflixoriginals'  fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title='Trending'  fetchUrl={requests.fetchTrending}/>
+      <Row title='Toprated'  fetchUrl={requests.fetchTopRated}/>
+      <Row title='Action Movies'  fetchUrl={requests.fetchActionMovies}/>
+      <Row title='Comedy Movies'  fetchUrl={requests.fetchComedyMovies}/>
+      <Row title='Horror Movies'  fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title='Romance Movies'  fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title='Documentaries'  fetchUrl={requests.fetchDocumentaries}/>
+
+      
+      
     </div>
   );
 }
---
+++ src/Components/Row.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import instance from '../instance';
+import './Row.css'
+
+function Row({ title, fetchUrl }) {
+  const base_url = "https://image.tmdb.org/t/p/original";
+  const [allMovies,setAllMovies]=useState([])
+  const fetchData = async () => {
+    const response = await instance.get(fetchUrl)
+    const {data}=response;
+    setAllMovies(data.results)
+  }
+  useEffect(() => {
+    fetchData();
+  },[]);
+  return (
+    <div className='row'>
+      <h2 style={{color:'white' }}>{title}</h2>
+    <div className='movie_row'>{
+      allMovies?.map(item =>(
+        <img src={`${base_url}${item.poster_path}`} className='movie'></img>
+      ))
+}
+    </div>
+    </div>
+  )
+}
+
+export default Row;
